038ba3a0cf93a40ec984f2c9c8c3c575a2201ec6
Wade Lieurance <wlieurance@gmail.com>
2020-11-04 23:22:21 -0800

    initital commit

---
+++ utils/update.sql
@@ -0,0 +1,6 @@
+UPDATE dnd_pub SET link = "/home/wade/DM/sourcebooks/5e/D&D 5E - Players Handbook.pdf", page_adjust = 1 WHERE pubkey = 'phb5e';
+UPDATE dnd_pub SET link = "/home/wade/DM/sourcebooks/5e/D&D 5E - Dungeon Masters Guide.pdf", page_adjust = 0 WHERE pubkey = 'dmg5e';
+UPDATE dnd_pub SET link = "/home/wade/DM/sourcebooks/5e/D&D 5E - Monster Manual.pdf", page_adjust = 1 WHERE pubkey = 'mm5e';
+UPDATE dnd_pub SET link = "/home/wade/DM/sourcebooks/5e/D&D 5E - Hoard of the Dragon Queen with Supplement.pdf", page_adjust = 1 WHERE pubkey = 'hdq5e';
+UPDATE dnd_pub SET link = "/home/wade/DM/sourcebooks/5e/D&D 5E - The Rise of Tiamat.pdf", page_adjust = 1 WHERE pubkey = 'rot5e';
+
